c72c42ac869ee35804c559530d2465cb691d0050
David Keller <mrcodeswildride@gmail.com>
2020-06-19 13:03:25 +0000

    es6 update

--- script.js
+++ script.js
@@ -1,105 +1,122 @@
-var rows = document.querySelectorAll(".row");
-var holes = document.querySelectorAll(".hole");
-var messageDisplay = document.getElementById("message");
+let rows = document.getElementsByClassName(`row`)
+let squares = document.getElementsByClassName(`square`)
+let messageParagraph = document.getElementById(`messageParagraph`)
 
-var gameInProgress = true;
-var pegSelected = null;
+let gameOver = false
+let selectedSquare
 
-for (var i = 0; i < holes.length; i++) {
-    holes[i].addEventListener("click", clickHole);
+for (let square of squares) {
+  square.addEventListener(`click`, clickSquare)
 }
 
-function clickHole() {
-    if (gameInProgress) {
-        if (!pegSelected) {
-            if (this.classList.contains("peg")) {
-                this.classList.add("selected");
-                pegSelected = this;
-            }
-        }
-        else {
-            if (this == pegSelected) {
-                pegSelected.classList.remove("selected");
-                pegSelected = null;
-            }
-            else if (this.classList.contains("peg")) {
-                pegSelected.classList.remove("selected");
-
-                this.classList.add("selected");
-                pegSelected = this;
-            }
-            else {
-                if (isTwoAway(this)) {
-                    var middle = getMiddle(this);
-
-                    if (middle.classList.contains("peg")) {
-                        pegSelected.classList.remove("selected");
-                        pegSelected.classList.remove("peg");
-                        pegSelected = null;
-
-                        middle.classList.remove("peg");
-                        this.classList.add("peg");
-
-                        if (isWin()) {
-                            gameInProgress = false;
-                            messageDisplay.innerHTML = "Good job!";
-                        }
-                    }
-                }
-            }
-        }
+function clickSquare() {
+  if (!gameOver) {
+    if (selectedSquare == null) {
+      if (this.classList.contains(`peg`)) {
+        selectedSquare = this
+        selectedSquare.classList.add(`selected`)
+      }
     }
+    else if (selectedSquare == this) {
+      selectedSquare.classList.remove(`selected`)
+      selectedSquare = null
+    }
+    else if (this.classList.contains(`peg`)) {
+      selectedSquare.classList.remove(`selected`)
+
+      selectedSquare = this
+      selectedSquare.classList.add(`selected`)
+    }
+    else if (isTwoAway(this)) {
+      let middle = getMiddle(this)
+
+      if (middle.classList.contains(`peg`)) {
+        selectedSquare.classList.remove(`selected`, `peg`)
+        selectedSquare = null
+
+        middle.classList.remove(`peg`)
+        this.classList.add(`peg`)
+
+        if (isSolved()) {
+          messageParagraph.innerHTML = `Good job!`
+          gameOver = true
+        }
+      }
+    }
+  }
 }
 
-function isTwoAway(hole) {
-    var pegX = parseInt(pegSelected.getAttribute("index"), 10);
-    var pegY = parseInt(pegSelected.parentElement.getAttribute("index"), 10);
+function isTwoAway(square) {
+  let squareTwoLeft = getNeighbor(selectedSquare, -2, 0)
+  let squareTwoRight = getNeighbor(selectedSquare, 2, 0)
+  let squareTwoAbove = getNeighbor(selectedSquare, 0, -2)
+  let squareTwoBelow = getNeighbor(selectedSquare, 0, 2)
 
-    var holeX = parseInt(hole.getAttribute("index"), 10);
-    var holeY = parseInt(hole.parentElement.getAttribute("index"), 10);
-
-    return (pegX == holeX && Math.abs(pegY - holeY) == 2) || (pegY == holeY && Math.abs(pegX - holeX) == 2);
+  return square == squareTwoLeft || square == squareTwoRight ||
+    square == squareTwoAbove || square == squareTwoBelow
 }
 
-function getMiddle(hole) {
-    var pegX = parseInt(pegSelected.getAttribute("index"), 10);
-    var pegY = parseInt(pegSelected.parentElement.getAttribute("index"), 10);
+function getMiddle(square) {
+  if (square == getNeighbor(selectedSquare, -2, 0)) {
+    return getNeighbor(selectedSquare, -1, 0)
+  }
+  else if (square == getNeighbor(selectedSquare, 2, 0)) {
+    return getNeighbor(selectedSquare, 1, 0)
+  }
+  else if (square == getNeighbor(selectedSquare, 0, -2)) {
+    return getNeighbor(selectedSquare, 0, -1)
+  }
+  else if (square == getNeighbor(selectedSquare, 0, 2)) {
+    return getNeighbor(selectedSquare, 0, 1)
+  }
+}
 
-    var holeX = parseInt(hole.getAttribute("index"), 10);
-    var holeY = parseInt(hole.parentElement.getAttribute("index"), 10);
+function getNeighbor(square, xDiff, yDiff) {
+  let row = square.parentElement // row of square
+  let y // y coordinate of square, set below
+  let x // x coordinate of square, set below
 
-    if (pegX == holeX) {
-        if (pegY < holeY) {
-            return rows[pegY + 1].querySelectorAll("div")[pegX];
-        }
-        else {
-            return rows[pegY - 1].querySelectorAll("div")[pegX];
-        }
+  // loop through rows to determine y
+  for (let i = 0; i < rows.length; i++) {
+    if (rows[i] == row) {
+      y = i // found matching row, so set y
+    }
+  }
+
+  // loop through squares in row to determine x
+  for (let i = 0; i < row.children.length; i++) {
+    if (row.children[i] == square) {
+      x = i // found matching square, so set x
+    }
+  }
+
+  // row of neighbor square
+  let neighborRow = rows[y + yDiff]
+
+  if (neighborRow == null) {
+    // row is beyond edge, so no neighbor square
+    return null
+  }
+  else {
+    // if x + xDiff is beyond edge, will be null
+    return neighborRow.children[x + xDiff]
+  }
+}
+
+function isSolved() {
+  for (let i = 0; i < squares.length; i++) {
+    if (i == 16) {
+      if (!squares[i].classList.contains(`peg`)) {
+        return false
+      }
     }
     else {
-        if (pegX < holeX) {
-            return rows[pegY].querySelectorAll("div")[pegX + 1];
-        }
-        else {
-            return rows[pegY].querySelectorAll("div")[pegX - 1];
-        }
-    }
-}
-
-function isWin() {
-    for (var i = 0; i < holes.length; i++) {
-        if (i == 16) {
-            if (!holes[i].classList.contains("peg")) {
-                return false;
-            }
-        }
-        else {
-            if (holes[i].classList.contains("peg")) {
-                return false;
-            }
-        }
-
+      if (squares[i].classList.contains(`peg`)) {
+        return false
+      }
     }
 
-    return true;
+  }
+
+  return true
 }
